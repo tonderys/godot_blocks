0849f9613c456822982d43cb20c1b7da0f92d167
tonderys <tonszym@gmail.com>
2022-02-08 18:17:47 +0100

    added squares flying up

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -1,9 +1,12 @@
 extends Node
 
 const Row = preload("res://scripts/Row.gd")
+const tick : float = 1.0/60
 onready var tileMap := get_node("TileMap")
 
 var rows = Array()
+var looseRows = Array()
+var elapsedTime = 0.0
 
 func _ready():
 	for height in range(0, 7):
@@ -17,8 +20,18 @@ func _input(event):
 		if event.is_pressed():
 			print("pressed at ", column)
 		else:
-			print("released at ", column)
-		
+			var row = Row.new(14, [column])
+			looseRows.append(row)
+			add_child(row)
+
+func _process(delta: float):
+	elapsedTime += delta
+	while elapsedTime > tick:
+		elapsedTime -= tick
+		for row in looseRows:
+			row.elevate(rows)
+			
+
 
 func onTimeout():
 	for row in rows:
--- scripts/Row.gd
+++ scripts/Row.gd
@@ -1,9 +1,13 @@
 extends Node2D
+class_name Row
 
 const Square = preload("res://scenes/Square.tscn")
+const square_side : int = 58
+const square_dimensions = Vector2(square_side, square_side)
 var rng = RandomNumberGenerator.new()
 
 var squares = Array()
+var height : int
 
 func randomIndices() -> Array:
 	var indices = [0,1,2,3,4,5,6,7,8,9]
@@ -11,15 +15,34 @@ func randomIndices() -> Array:
 	indices.resize(rng.randi_range(2, 9))
 	return indices
 
-func _init(height: int):
-	self.position = Vector2(0, height * 58)
+func _init(height: int, indices: Array = randomIndices()):
+	self.height = height
 	rng.randomize()
-	var indices = randomIndices()
 	for i in indices:
+		squares.append(i)
 		var square = Square.instance()
-		square.get_node("body").rect_size = Vector2(58,58)
-		square.get_node("body").rect_position = Vector2(i * 58, 0)
+		square.get_node("body").rect_size = square_dimensions
+		square.get_node("body").rect_position = Vector2(i * square_side, 0)
 		add_child(square)
 
-func lower():
-	self.position.y += 58
+func _process(_delta: float):
+	self.position = Vector2(0, self.height * square_side)
+
+func canMergeWith(other: Row) -> bool:
+	for otherSquare in other.squares:
+		for ownSquare in squares:
+			if ownSquare == otherSquare:
+				return false
+	return true
+
+func mergeWith(other: Row) -> void:
+	for square in other.squares:
+		squares.append(square)
+	for child in other.get_children():
+		add_child(child)
+
+func lower() -> void:
+	self.height += 1
+
+func elevate(rows: Array) -> void:
+	self.height -= 1
