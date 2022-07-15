2676fb6904327c2484890427ef4c17fe495456c1
tonderys <tonszym@gmail.com>
2022-07-15 08:47:29 +0200

    added multiplier indicator and implemented functionality that prevents optimizing fun out of the game (lvl up after 100 new rows)

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -6,15 +6,22 @@ onready var boardNode = get_node("BoardNode")
 var elapsedTime = 0.0
 const tick : float = 1.0/60
 var level = 1
+var rowsToNextLevel = 100
 
 func _init():
 	Global.score = 0
 
 func _process(_delta: float) -> void:
-	get_node("Score").text = "Score:%s" % Global.score
 	if boardNode.isEmpty():
+		rowsToNextLevel = 100
 		level += 1
+		get_node("Multipier").text = "x%s" % level
+		Global.shortenTimer(get_node("Timer/remaining"), 0.9)
 		boardNode.reset()
+	elif rowsToNextLevel <= 0:
+		rowsToNextLevel = 100
+		level += 1
+		get_node("Multipier").text = "x%s" % level
 		Global.shortenTimer(get_node("Timer/remaining"), 0.9)
 	elif boardNode.isFull():
 		get_tree().change_scene("res://scenes/Summary.tscn")
@@ -23,7 +30,7 @@ func _process(_delta: float) -> void:
 	while elapsedTime > tick:
 		elapsedTime -= tick
 		boardNode.elevateLooseRows()
-		
+
 func _input(event):
 	if event is InputEventMouseButton:
 		var column = int(event.position.x / 58)
@@ -32,5 +39,10 @@ func _input(event):
 		else:
 			boardNode.input(column)
 
+func onTimeout():
+	rowsToNextLevel -= 1
+	boardNode.addTopRow()
+
 func addPoints(multiplier: int):
 	Global.score += 10 * level * multiplier
+	get_node("Score").text = "Score:%s" % Global.score
