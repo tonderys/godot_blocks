4f8b391ce360681705af3c0e0a0991524ef22252
Szymon <tonszym@gmail.com>
2023-03-03 08:22:39 +0100

    first step for showing players scores
    ...also added reminder for the global scores

--- scripts/MenuButtons.gd
+++ scripts/MenuButtons.gd
@@ -8,13 +8,15 @@ func quit_pressed():
 	get_tree().paused = false
 	get_tree().quit()
 
-func settings_pressed():
-	get_tree().paused = false
-	get_tree().change_scene("res://scenes/Settings.tscn")
-
 func back_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://scenes/Menu.tscn")
 
 func toggle_pause():
 	get_owner().toggle_pause()
+
+func settings_pressed():
+	get_tree().change_scene("res://scenes/Settings.tscn")
+
+func highscores_pressed():
+	get_tree().change_scene("res://scenes/Scoreboard.tscn")
