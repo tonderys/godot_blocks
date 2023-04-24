fd7e38d4f5fa5f427c7959245e8e89e850814af5
Szymon <tonszym@gmail.com>
2023-04-24 09:11:32 +0200

    a little hacky ensure menu
    after pressing back or restart, there's a menu asking if the user is sure

--- scripts/MenuButtons.gd
+++ scripts/MenuButtons.gd
@@ -1,5 +1,8 @@
 extends Node
 
+enum Action{NONE = 0, BACK = 1, RESTART = 2}
+var action = Action.NONE
+
 func start_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://scenes/Game.tscn")
@@ -11,6 +14,25 @@ func quit_pressed():
 func back_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://scenes/Menu.tscn")
+
+func ensure(text, act):
+	$menu.visible = false
+	var prompt = get_node("ensure prompt")
+	prompt.visible = true
+	prompt.get_node("option").text = text
+	action = act
+
+func yes_pressed():
+	if action == Action.RESTART:
+		start_pressed()
+	if action == Action.BACK:
+		back_pressed()
+
+func restore_pause_menu():
+	var prompt = get_node("ensure prompt")
+	prompt.visible = false
+	$menu.visible = true
+	action = Action.NONE
 
 func toggle_pause():
 	get_owner().toggle_pause()
